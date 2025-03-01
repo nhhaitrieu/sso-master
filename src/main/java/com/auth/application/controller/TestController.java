fc04c12b4f1aea86cd7b366cda5d29ec33a50ffe
Hachi <Hachi@MacBook-Air.local>
2025-03-01 17:35:53 +0800

    Initialize for SSO project

---
+++ src/main/java/com/auth/application/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.auth.application.controller;
+
+import com.auth.application.model.TestModel;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello Word";
+    }
+
+    @PostMapping("/demo")
+    public String demo(@RequestParam("name") String name) {
+        TestModel testModel = new TestModel(name, "18");
+        testModel.setName(name);
+        return "Hello Word " + testModel.getName();
+    }
+}
---
+++ src/main/java/com/auth/application/model/TestModel.java
@@ -0,0 +1,11 @@
+package com.auth.application.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestModel {
+    private String name;
+    private String age;
+}
